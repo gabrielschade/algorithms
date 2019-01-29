9d3f279337be41cf5e1c21aded99af1e8fb006e7
Gabriel Schade <gabriel.schade@hotmail.com>
2019-01-29 14:01:24 -0200

    Post sobre recursão em cauda

---
+++ TailRecursion/Program.fs
@@ -0,0 +1,28 @@
+﻿open System
+
+let helloFunction() =
+    printf "Hello"
+
+let rec sum list =
+    match list with
+    | [] -> 0
+    | head::tail -> head + sum tail
+
+let tailRecursionSum list =
+    let rec _internalSum list acc =
+        match list with
+        | [] -> acc
+        | head::tail -> _internalSum tail (acc+head)
+
+    _internalSum list 0
+
+[<EntryPoint>]
+let main argv =  
+    List.init 100000 (fun index -> index)
+    |> tailRecursionSum
+    |> Console.WriteLine
+
+    Console.ReadKey()
+    |> ignore
+
+    0
