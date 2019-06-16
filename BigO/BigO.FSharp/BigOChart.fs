1e4dd957815d58655931a2932e4f29a53bda54d7
Gabriel Schade <gabriel.schade@hotmail.com>
2019-06-16 18:03:32 +0100

    Separação da função que gera o gráfico;

---
+++ BigO/BigO.FSharp/BigOChart.fs
@@ -0,0 +1,40 @@
+﻿module BigOChart
+
+open System
+open XPlot.GoogleCharts
+open System.IO
+open System.Diagnostics
+
+let generateBigOGraph =
+    let baseList = [for number in 1..1000 do yield (float)number]
+    let o1 = baseList |> List.map(fun n -> n.ToString(), 1.0)
+    let oLogN = baseList |> List.map( fun n -> n.ToString(), Math.Log(n,2.0))
+    let oSquareRootN = baseList |> List.map (fun n -> n.ToString(), Math.Sqrt(n))
+    let oN = baseList |> List.map(fun n -> n.ToString(), n)
+    let oNLogN = baseList |> List.map(fun n -> n.ToString(), n * (Math.Log(n,2.0)))
+    let oSquared = baseList |> List.map( fun n -> n.ToString(), n * n)
+    let o2PoweredN = baseList |> List.map( fun n -> n.ToString(), Math.Pow(2.0, n))
+    let options = Options ( 
+                            title = "Big-O", 
+                            curveType = "function", 
+                            legend = Legend(position = "bottom"),
+                            vAxis = Axis ( 
+                                maxValue = 50
+                            )
+    )
+    
+
+    let chart = 
+        [o1;oLogN;oSquareRootN; oN; oNLogN; oSquared; o2PoweredN]
+        |> Chart.Line
+        |> Chart.WithOptions options
+        |> Chart.WithHeight 400
+        |> Chart.WithYTitle "Number of Interactions"
+        |> Chart.WithLabels ["O(1)";"O(log n)"; "O(√n)";"O(n)";"O(n log n)";"O(n^2)";"O(2^n)"]
+    
+    let html = chart.GetHtml()
+    File.AppendAllLines ("metrics.html",[html])
+    
+    Process.Start (@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", 
+                    "file:\\" + Directory.GetCurrentDirectory() + "\\bigO.html")
+    |> ignore  
--- BigO/BigO.FSharp/Program.fs
+++ BigO/BigO.FSharp/Program.fs
@@ -1,10 +1,4 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
-open Microsoft.FSharp.Linq
-open XPlot.GoogleCharts
-open System.IO
-open System.Diagnostics
+﻿open System
 
 let isEvenAt index (list : int list) =
     list.[index] % 2 = 0
@@ -43,55 +37,11 @@ let binarySearch value (list:int list)=
 
     containsValueAt value list 0 list.Length
 
-let valueOrNull value =
-    match value with
-    | n when n > 100.0 -> Nullable()
-    | _ -> Nullable(value)
-
-let generateBigOGraph =
-    let baseList = [for number in 1..1000 do yield (float)number]
-    let o1 = baseList |> List.map(fun n -> n.ToString(), 1.0)
-    let oLogN = baseList |> List.map( fun n -> n.ToString(), Math.Log(n,2.0))
-    let oSquareRootN = baseList |> List.map (fun n -> n.ToString(), Math.Sqrt(n))
-    let oN = baseList |> List.map(fun n -> n.ToString(), n)
-    let oNLogN = baseList |> List.map(fun n -> n.ToString(), n * (Math.Log(n,2.0)))
-    let oSquared = baseList |> List.map( fun n -> n.ToString(), n * n)
-    let o2PoweredN = baseList |> List.map( fun n -> n.ToString(), Math.Pow(2.0, n))
-    let options = Options ( 
-                            title = "Big-O", 
-                            curveType = "function", 
-                            legend = Legend(position = "bottom"),
-                            vAxis = Axis ( 
-                                maxValue = 50
-                            )
-    )
-    
-
-    let chart = 
-        [oLogN; ]//oLogN;oSquareRootN; oN; ]//oNLogN; ]//oSquared;]// o2PoweredN]
-        |> Chart.Line
-        |> Chart.WithOptions options
-        |> Chart.WithHeight 400
-        |> Chart.WithYTitle "Number of Interactions"
-        |> Chart.WithLabels ["O(log n)";]// "O(log n)"; "O(√n)";"O(n)"]//;"O(n log n)"]//;"O(n^2)"]//;"O(2^n)"]
-    
-    let html = chart.GetHtml()
-    File.AppendAllLines ("metrics.html",[html])
-    
-    Process.Start (@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", 
-                    "file:\\" + Directory.GetCurrentDirectory() + "\\metrics.html")
-    |> ignore  
-    0
-
 [<EntryPoint>]
 let main argv =
     let list = [for number in 1..100 do yield number]
     list
     |> binarySearch 40
-    |> Console.WriteLine
+    |> Console.WriteLine 
 
-
-    generateBigOGraph |> ignore
-    
-
-    0 // return an integer exit code
+    0
